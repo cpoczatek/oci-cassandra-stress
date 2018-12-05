6a85c7b1daa08a7eca9ec7da96924444222b7f1c
Collin Poczatek <collin.poczatek@gmail.com>
2018-12-05 10:25:34 -0800

    oracle linux images

--- variables.tf
+++ variables.tf
@@ -41,14 +41,14 @@ variable "nodes" {}
 # You probably don't need to change these.
 # ---------------------------------------------------------------------------------------------------------------------
 
-// https://docs.cloud.oracle.com/iaas/images/image/67026570-4527-42f5-b483-75085379e48c/
-// Canonical-Ubuntu-16.04-2018.08.15-0
+// https://docs.cloud.oracle.com/iaas/images/image/cf34ce27-e82d-4c1a-93e6-e55103f90164/
+// Oracle-Linux-7.5-2018.08.14-0
 variable "images" {
   type = "map"
   default = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3tl3phbgdyzjba64h2tgunnwzxbxtw4r36u4ttnsn3m77t6jcurq"
-    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaa7keb3ok2deynxzsz7k5rondhuc7nt5vw6hf3q5xslyiepnqsi3aq"
-    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaafrt5olkogiw2xn74ssu4mjnv7e2wgqkmxwuo4kqihggt74bmgpza"
-    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaaizzbceqxadaggbchf6yasjralcqdlh2tmgca6ag5b4gvcg4k4rha"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaakzrywmh7kwt7ugj5xqi5r4a7xoxsrxtc7nlsdyhmhqyp7ntobjwq"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaa2tq67tvbeavcmioghquci6p3pvqwbneq3vfy7fe7m7geiga4cnxa"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaasez4lk2lucxcm52nslj5nhkvbvjtfies4yopwoy4b3vysg5iwjra"
+    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaalsdgd47nl5tgb55sihdpqmqu2sbvvccjs6tmbkr4nx2pq5gkn63a"
   }
 }
